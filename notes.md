a5601668c43b00662544b9aa1ca52ca1db27f7bf
Pablo Valenzuela <p.valenzuela17@gmail.com>
2020-05-14 15:24:28 -0500

    Hello

--- notes.md
+++ notes.md
@@ -14,3 +14,11 @@ $()
 * Descendants - `$('ul li')`; 
 * Multpiple elements - `$('.mic', '.check').append('test test')`
 * Find elements - `$('#solid').find('.rock')`; 
+
+
+
+## DOM TRAVERSAL 
+* parent element - `$('.child').parent()';
+* first-level children - `$('.parent').children()'
+
+##DOM MANIPULATION  
--- scripts.js
+++ scripts.js
@@ -7,9 +7,15 @@ $(document).ready(onReady);
 function onReady() {
     console.log('JQuary is loaded')
     $('#hello').append('welcome to my page!'); 
-    $()
+   // $('#hello').css('color', 'pink');
+   $('#hello').addClass('blue'); 
+   $('#submit').on('click', handleSumbmit); 
 }
 
 
 // 1st event code runs at 
 console.log("hello"); 
+
+function handleSum(params) {
+    
+}
